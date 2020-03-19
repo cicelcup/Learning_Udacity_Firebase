6dddef89fd4ea53a0ab29429a69104cc9c49f9f7
JAPM <cicelcup@gmail.com>
2020-03-19 03:04:53 -0600

    Change activity layout

--- app/src/main/java/com/example/testing_firebasedb/MainActivity.kt
+++ app/src/main/java/com/example/testing_firebasedb/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.testing_firebasedb
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.testing_firebasedb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil
+            .setContentView(this, R.layout.activity_main)
     }
 }
