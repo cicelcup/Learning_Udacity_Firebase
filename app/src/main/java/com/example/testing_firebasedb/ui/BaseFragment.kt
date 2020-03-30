6f4e90c5091c3fc1887cb2c4394750aaf1b106a5
JAPM <cicelcup@gmail.com>
2020-03-30 10:26:45 -0600

    Add menu top right in the screen

--- app/src/main/java/com/example/testing_firebasedb/ui/BaseFragment.kt
+++ app/src/main/java/com/example/testing_firebasedb/ui/BaseFragment.kt
@@ -3,16 +3,19 @@ package com.example.testing_firebasedb.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.testing_firebasedb.R
 import com.example.testing_firebasedb.adapters.MessagesAdapter
 import com.example.testing_firebasedb.data.AuthenticationState
 import com.example.testing_firebasedb.databinding.FragmentBaseBinding
 import com.example.testing_firebasedb.mvvm.MessagesVM
+import com.google.firebase.auth.FirebaseAuth
 
 class BaseFragment : Fragment() {
 
@@ -28,6 +31,11 @@ class BaseFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentBaseBinding.inflate(layoutInflater, container, false)
+        //Inflate the menu of the fragment
+        with(binding.toolbar) {
+            inflateMenu(R.menu.top_right_menu)
+            setOnMenuItemClickListener { onOptionsItemSelected(it) }
+        }
         return binding.root
     }
 
@@ -55,5 +63,15 @@ class BaseFragment : Fragment() {
                 Log.i(TAG, "User authenticated")
             }
         })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.signOutButton -> {
+                FirebaseAuth.getInstance().signOut()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
